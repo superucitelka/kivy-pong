cbbad839378b95216e6b44cfe10c95648d2caf44
superucitelka <dokonala.hilda@gmail.com>
2021-12-09 09:11:10 +0100

    03 - Screen manager a soubor .kv

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Pong ve frameworku Kivy
 - 01 - Hlavní okno aplikace
 - 02 - Konfigurace okna (https://kivy.org/doc/stable/api-kivy.config.html)
+- 03 - Screen manager a soubor .kv (https://kivy.org/doc/stable/api-kivy.uix.screenmanager.html)
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+# Třída pro obrazovku s menu
+class MenuScreen(Screen):
+    pass
+
+
+ # Třída pro obrazovku s plátnem - budoucí hrací plocha
+class CanvasScreen(Screen):
+    pass
 
 
 class PongApp(App):
     def build(self):
+        # Vytvoření objektu ScreenManageru do proměnné sm (https://kivy.org/doc/stable/api-kivy.uix.screenmanager.html)
+        sm = ScreenManager()
+        # Přidání dvou widgetů-obrazovek do sm
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(CanvasScreen(name='canvas'))
+
         # Konfigurace rozměrů hlavního okna aplikace
         # viz https://kivy.org/doc/stable/api-kivy.config.html
         Config.set('graphics', 'width', '800')
@@ -13,5 +30,7 @@ class PongApp(App):
         Config.set('graphics', 'resizable', False)
         Config.write()
         Window.size = (800, 600)
+        # Vrací objekt sm - zobrazí se základní GUI aplikace
+        return sm
 
 PongApp().run()
