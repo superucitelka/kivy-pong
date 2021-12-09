828a99982ca69b33ca24cbea4d77111352a0d465
superucitelka <dokonala.hilda@gmail.com>
2021-12-09 08:52:59 +0100

    02 - Konfigurace okna

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pong ve frameworku Kivy
 - 01 - Hlavní okno aplikace
+- 02 - Konfigurace okna (https://kivy.org/doc/stable/api-kivy.config.html)
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from kivy.app import App
+from kivy.config import Config
+from kivy.core.window import Window
 
 
 class PongApp(App):
     def build(self):
-        pass
+        # Konfigurace rozměrů hlavního okna aplikace
+        # viz https://kivy.org/doc/stable/api-kivy.config.html
+        Config.set('graphics', 'width', '800')
+        Config.set('graphics', 'height', '600')
+        # Nebude možné měnit velikost hlavního okna
+        Config.set('graphics', 'resizable', False)
+        Config.write()
+        Window.size = (800, 600)
 
 PongApp().run()
